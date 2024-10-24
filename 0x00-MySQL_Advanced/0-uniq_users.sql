0ba72054837e3375bb915d427ac926fc37bb2caf
EuniceGeorge <georgeunice408.com>
2024-10-24 16:07:56 +0100

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,10 @@
 -- a SQL script that creates a table users
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
 CREATE TABLE IF NOT EXISTS users (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL,
 	name VARCHAR(255),
 	UNIQUE (email),
